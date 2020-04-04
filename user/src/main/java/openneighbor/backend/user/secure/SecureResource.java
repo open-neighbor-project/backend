dbb6fca0c633513a01397442c7f7bf98e5425a23
DannyS03 <danny.salman@ryerson.ca>
2020-04-04 17:30:38 -0400

    get userid from jwt

--- user/src/main/java/openneighbor/backend/user/secure/SecureResource.java
+++ user/src/main/java/openneighbor/backend/user/secure/SecureResource.java
@@ -1,35 +1,26 @@
 package openneighbor.backend.user.secure;
 
-import javax.annotation.security.RolesAllowed;
-import javax.ejb.Stateless;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
+import javax.annotation.security.RolesAllowed;
+
+@RequestScoped
 @Path("secure")
-@Stateless
 public class SecureResource {
+  
+  @Inject
+  private JsonWebToken jwtPrincipal;
 
-	@Inject
-	@ConfigProperty(name = "message")
-	private String message;
-
-	@Inject
-	private JsonWebToken callerPrincipal;
-
-	@GET
-	@RolesAllowed("Volunteer")
-	public Response message() {
-
-		System.out.println(callerPrincipal.getIssuer());
-		System.out.println(callerPrincipal.getRawToken());
-		System.out.println(callerPrincipal.getTokenID());
-
-		return Response.ok(callerPrincipal.getName() + " is allowed to read message: " + message).build();
-	}
+  @GET
+  @RolesAllowed({ "requester", "volunteer" })
+  @Path("/userid")
+  public Response getJwtId() {
+    return Response.ok(this.jwtPrincipal.getName()).build();
+  }
 }
-
